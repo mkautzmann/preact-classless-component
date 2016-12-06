dfa5838ea1fcf579dd5745a7419ce2644f22d2a6
Matheus Kautzmann <kautzmann5@gmail.com>
2016-12-06 09:54:30 -0200

    Export UMD

--- dist/index.js
+++ dist/index.js
@@ -1,32 +1,58 @@
-'use strict';
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['module', 'exports', 'preact'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(module, exports, require('preact'));
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod, mod.exports, global.preact);
+    global.preactComponent = mod.exports;
+  }
+})(this, function (module, exports, _preact) {
+  'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+  exports.default = function (obj) {
+    function preactComponent() {
+      _preact.Component.apply(this, arguments);
 
-exports.default = function (obj) {
-  function preactComponent() {
-    _preact.Component.apply(this, arguments);
+      // auto-bind methods to the component
+      for (var i in obj) {
+        if (i !== 'render' && typeof obj[i] === 'function') {
+          this[i] = obj[i].bind(this);
+        }
+      }
 
-    // auto-bind methods to the component
-    for (var i in obj) {
-      if (i !== 'render' && typeof obj[i] === 'function') {
-        this[i] = obj[i].bind(this);
+      if (obj.init) {
+        obj.init.call(this);
       }
     }
 
-    if (obj.init) {
-      obj.init.call(this);
+    preactComponent.prototype = _extends(Object.create(_preact.Component.prototype), obj);
+
+    preactComponent.prototype.constructor = preactComponent;
+
+    return preactComponent;
+  };
+
+  var _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
     }
-  }
 
-  preactComponent.prototype = _extends(Object.create(_preact.Component.prototype), obj);
+    return target;
+  };
 
-  preactComponent.prototype.constructor = preactComponent;
-
-  return preactComponent;
-};
-
-var _preact = require('preact');
+  module.exports = exports['default'];
+});
